dda4660270db7dd02df370c8ca85717c9d75135f
Pablo Estobar Fernández <56236283+Imperialdramon@users.noreply.github.com>
2025-01-28 14:37:53 -0300

    Added PSO-X runs

--- Execute_Runs_Irace.py
+++ Execute_Runs_Irace.py
@@ -15,11 +15,8 @@ def execute_scenario(path: str, seed: int, id_scenario: int, train_dir: str, par
         train_dir (str): Directorio relativo donde se encuentran las instancias.
     """
     scenario_file = os.path.join(path, 'scenario.txt')
-    relative_train_dir = os.path.relpath(train_dir, start=path)
     
     with open(scenario_file, 'a') as file:
-        file.write("\n## Folder of instances\n")
-        file.write(f'trainInstancesDir="{relative_train_dir}"\n')
         file.write("\n## Seed\n")
         file.write(f"seed={seed}\n")
         file.write("\n## Scenario ID\n")
@@ -27,27 +24,28 @@ def execute_scenario(path: str, seed: int, id_scenario: int, train_dir: str, par
         file.write("\n## Number of threads\n")
         file.write(f"parallel={parallel}\n")
 
-    command = "Rscript execute_irace.r > output.log"
+    command = "Rscript execute_irace.R > output.log"
     subprocess.run(command, shell=True, cwd=path)
 
     print(f"Escenario {id_scenario} terminado: {path}, seed={seed}")
 
-# Directorios de escenarios de base, de destino, nombre de los escenarios y ubicación de las instancias de entrenamiento
+# Directorios de escenarios de base, de destino y nombre de los escenarios
 directories = [
-    ['Runs/ACOTSP-Normal/Base', 'Runs/ACOTSP-Normal/Run', 'Normal', 'Runs/ACOTSP-Normal/Instances'],
-    ['Runs/ACOTSP-Sr/Base', 'Runs/ACOTSP-Sr/Run', 'Normal', 'Runs/ACOTSP-Sr/Instances'],
-    ['Runs/MMASQAP-Normal/Base', 'Runs/MMASQAP-Normal/Run', 'Normal', 'Runs/MMASQAP-Normal/Instances'],
-    ['Runs/MMASQAP-Sr/Base', 'Runs/MMASQAP-Sr/Run', 'Normal', 'Runs/MMASQAP-Sr/Instances'],
+#    ['Runs/ACOTSP-N/Base', 'Runs/ACOTSP-N/Run', 'N'],
+#    ['Runs/ACOTSP-SR/Base', 'Runs/ACOTSP-SR/Run', 'SR'],
+#    ['Runs/MMASQAP-N/Base', 'Runs/MMASQAP-N/Run', 'N'],
+#    ['Runs/MMASQAP-SR/Base', 'Runs/MMASQAP-SR/Run', 'SR'],
+    ['Runs/PSOX-Mixed-N/Base', 'Runs/PSOX-Mixed-N/Run', 'N'],
 ]
 
 # Semillas para cada combinación
-seeds = [7940, 9411, 7175, 9685, 7018, 1569, 128, 5144, 8860, 3764]
+seeds = [7940, 9411] #, 7175, 9685, 7018, 1569, 128, 5144, 8860, 3764]"""
 
 # Listas para almacenar los datos de cada run
 runs_data = []
 
 # Generar combinaciones de escenarios base y semillas
-for base_dir, dest_dir, scenary_type, instances_dir in directories:
+for base_dir, dest_dir, scenary_type in directories:
     # Verificación de existencia de directorio base
     if not os.path.exists(base_dir):
         raise FileNotFoundError(f"El directorio base '{base_dir}' no existe.")
@@ -59,7 +57,7 @@ for base_dir, dest_dir, scenary_type, instances_dir in directories:
         path = os.path.join(dest_dir, f"{scenary_type}_seed_{seed}")
         os.makedirs(path, exist_ok=True)
         shutil.copytree(base_dir, path, dirs_exist_ok=True)
-        runs_data.append((path, seed, run_id, instances_dir))
+        runs_data.append((path, seed, run_id))
         run_id += 1
 
 # Configuración de paralelismo
@@ -73,7 +71,7 @@ print(f"Número de threads por escenario: {parallel}\n")
 
 # Ejecutar en paralelo
 with concurrent.futures.ThreadPoolExecutor(max_workers=K) as executor:
-    futures = {executor.submit(execute_scenario, path, seed, run_id, instances_dir, parallel): [path, seed, run_id, instances_dir] for path, seed, run_id, instances_dir in runs_data}
+    futures = {executor.submit(execute_scenario, path, seed, run_id, parallel): [path, seed, run_id] for path, seed, run_id in runs_data}
     print("Ejecutando escenarios...\n")
     for future in concurrent.futures.as_completed(futures):
         path, seed, run_id, _ = futures[future]
